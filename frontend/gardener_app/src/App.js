14c2553c66a45375318821202c03fc4650f7a6d2
kelly_maass <kmaass77627@gmail.com>
2024-08-24 11:03:03 -0500

    done

---
+++ frontend/gardener_app/src/App.js
@@ -0,0 +1,15 @@
+import Plant from "./components/Plant"
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <Plant/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/gardener_app/src/components/Plant.js
@@ -0,0 +1,11 @@
+
+
+function Plant() {
+    return (
+        <div>
+            <h1>hello plant</h1>
+        </div>
+    )
+}
+
+export default Plant;
