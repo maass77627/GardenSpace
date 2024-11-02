a551406ed1c0f3ca823d14ed39bbe7a1f0928a0e
kelly_maass <kmaass77627@gmail.com>
2024-11-02 18:16:09 -0500

    done

--- backend/app/models/gardener.rb
+++ backend/app/models/gardener.rb
@@ -3,14 +3,4 @@ require 'pry'
 class Gardener < ActiveRecord::Base
     has_many :plants
 
-    # @@all = []
-
-    # def initialize(name)
-    #     @name = name
-    #     @@all << self
-    # end
-    
-    
-
-
 end
